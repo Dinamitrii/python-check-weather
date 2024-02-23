f70ced31bfc604cc5fd707c1a07638225c019b7a
Dinamitrii <k_karaivanov@abv.bg>
2024-02-23 23:50:58 +0200

    Added info in README.md

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def get_current_weather(city="Sofia"):
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&lang=bg&units=metric'
 
     return requests.get(request_url).json()
 
