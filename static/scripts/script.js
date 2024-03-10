18b0c8bf606b97ad62320a338c9d626700ec1a20
dinamitrii <k_karaivanov@abv.bg>
2024-03-10 12:37:33 +0200

    adding extra files and info

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,5 @@
 function changeButtonText() {
     document.getElementById("button_weather").innerText="Delivered requested info"
-    console.log(document.getElementById("button_index").click());
+    console.log(document.getElementById("button_weather").click());
     return <textarea name="testArea" id="text_area" cols="3" rows="3">Text Area</textarea>;
 }
