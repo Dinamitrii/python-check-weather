f897fdd59414a921fbc2a2853cdeb489186ecfba
dinamitrii <k_karaivanov@abv.bg>
2024-03-10 12:41:46 +0200

    adding extra files and info

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,3 @@
 function changeButtonText() {
-    document.getElementById("button_weather").innerText="Delivered requested info"
-    console.log(document.getElementById("button_weather").click());
-    return <textarea name="testArea" id="text_area" cols="3" rows="3">Text Area</textarea>;
+    document.getElementById("button_weather").insertAdjacentText("afterend", "Delivered requested info");
 }
