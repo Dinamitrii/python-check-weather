e956479cfdf3e04fadafc2f15f725a92cfe48249
dinamitrii <k_karaivanov@abv.bg>
2024-03-10 05:54:13 +0200

    redoing in progress

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 
-def get_current_weather(city):
+def get_current_weather(city="Sofia"):
 
     request_url = (f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&lang=bg'
                    f'&units=metric')
