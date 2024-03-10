8db6ed18bd238fea19f7f8ea853cdc545f9ca88a
dinamitrii <k_karaivanov@abv.bg>
2024-03-10 12:17:20 +0200

    adding extra files and info

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,4 @@
 function changeButtonText() {
-    document.getElementById("button_weather").textContent="Delivered requested info"
-    document.getElementById("button_index").textContent="Delivered requested info"
+    document.getElementById("button_weather").innerText="Delivered requested info"
+    document.getElementById("button_index").innerText="Delivered requested info"
 }
