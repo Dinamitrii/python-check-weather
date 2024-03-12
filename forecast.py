01a2fddecedcf72247be4ea7b1defcc7e061642b
dinamitrii <k_karaivanov@abv.bg>
2024-03-12 21:59:41 +0200

    new feature "three days forecast" implementing in progress. Works at this time.

--- forecast.py
+++ forecast.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 
-def get_current_forecast(latitude, longitude):
+def get_current_forecast(latitude=42.6975, longitude=23.3242):
     forecast_request = (f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid="
                         f"{os.getenv('API_KEY')}&units=metric&lang=bg")
 
