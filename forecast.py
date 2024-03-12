9d2f030817e6be46c581dfcb3da5d50138cfa115
dinamitrii <k_karaivanov@abv.bg>
2024-03-12 23:13:03 +0200

    new feature "three days forecast" implementing in progress......

--- forecast.py
+++ forecast.py
@@ -6,24 +6,32 @@ import os
 load_dotenv()
 
 
-def get_current_forecast(latitude=42.6975, longitude=23.3242):
-    forecast_request = (f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid="
+def get_current_forecast(lat=42.6975, lon=23.3242):
+    forecast_request = (f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={longitude}&appid="
                         f"{os.getenv('API_KEY')}&units=metric&lang=bg")
+
+    print(forecast_request)
 
     return requests.get(forecast_request).json()
 
 
-get_current_forecast(latitude=42.6975, longitude=23.3242)
+get_current_forecast(lat=, lon=)
 
 print("\n*** Get Current Weather Forecast ***\n")
 
-latitude = float(input("\nPlease enter a latitude in degrees(float): "))
-longitude = float(input("\nPlease enter a longitude in degrees(float): "))
+latitude = input(f"\nPlease enter a latitude in degrees(float): ")
+longitude = input("\nPlease enter a longitude in degrees(float): ")
 
-if latitude == 0 and longitude is None:
-    latitude, longitude = 42.6975, 23.3242
+# Check for empty strings or string with only spaces
+if not bool(latitude.strip() or not bool(longitude.strip())):
 
-    forecast_data = get_current_forecast(latitude, longitude)
+    lat = float(latitude)
+    lon = float(longitude)
+
+    if lat is None or lon is None:
+        lat, lon = 42.6975, 23.3242
+
+    forecast_data = get_current_forecast(lat, lon)
 
     print("\n")
     pprint(forecast_data)
