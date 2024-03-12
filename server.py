3dbe4b6e9f8265ccfa0a1d79e1a36c12a47a80e0
dinamitrii <k_karaivanov@abv.bg>
2024-03-12 18:33:32 +0200

    new feature "three days forecast" implementing in progress...

--- server.py
+++ server.py
@@ -46,13 +46,18 @@ def get_weather():
 
 @app.route("/forecast/")
 def get_forecast(latitude, longitude):
+    
     if latitude and longitude == 0:
         latitude, longitude = 42.6975, 23.3242
 
     forecast_data = get_current_forecast(latitude, longitude)
 
-    # latitude = request.args.get("lat")
-    # longitude = request.args.get("lon")
+    latitude = request.args.get("lat")
+    longitude = request.args.get("lon")
+    
+    if forecast_data["cod"] != 200:
+        return render_template("city-not-found.html")
+
 
     return render_template(
         "forecast.html",
