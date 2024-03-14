9a6e2baaff4c5bb8c1a8e96e804f1884d17f94be
dinamitrii <k_karaivanov@abv.bg>
2024-03-14 07:42:04 +0200

    ...silenced...

--- forecast.py
+++ forecast.py
@@ -13,29 +13,25 @@ def get_current_forecast(lat=42.6975, lon=23.3242):
     return requests.get(forecast_request).json()
 
 
-get_current_forecast(lat=42.6975, lon=23.3242)
+if __name__ == "__main__":
+    # get_current_forecast(lat=42.6975, lon=23.3242)
 
-print("\n*** Get Current Weather Forecast ***\n")
+    print("\n*** Get Current Weather Forecast ***\n")
 
-latitude = input(f"\nPlease enter a latitude in degrees(float): ")
-longitude = input("\nPlease enter a longitude in degrees(float): ")
+    latitude = input(f"\nPlease enter a latitude in degrees(float): ")
+    longitude = input("\nPlease enter a longitude in degrees(float): ")
 
+    # Check for empty strings or string with only spaces
+    if not latitude == "" and not longitude == "":
 
-# Check for empty strings or string with only spaces
-if not latitude == "" and not longitude == "":
+        lat = float(latitude)
+        lon = float(longitude)
 
-    lat = float(latitude)
-    lon = float(longitude)
+    else:
+        lat = float(42.6975)
+        lon = float(23.3242)
 
-
-else:
-    lat = float(42.6975)
-    lon = float(23.3242)
-
-    # if latitude is None and longitude is None:
-    #     lat, lon = 42.6975, 23.3242
-
-    forecast_data = get_current_forecast(lat, lon)
+       forecast_data = get_current_forecast(lat, lon)
 
     print("\n")
     pprint(forecast_data)
