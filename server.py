37eb60b249f2a5207d82b068de1e942a40617d07
dinamitrii <k_karaivanov@abv.bg>
2024-06-15 11:15:02 +0300

    changes now

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for
 from datetime import datetime, timezone
+from timestamp import fromtimestamp
 from weather import get_current_weather
 from waitress import serve
 from dotenv import load_dotenv
@@ -30,6 +31,10 @@ def get_weather():
         return render_template("city-not-found.html")
 
     # If city is found by API
+    sunrise_timestamp = weather_data['sys']['sunrise']
+    sunset_timestamp = weather_data['sys']['sunset']
+    sunrise_date = fromtimestamp(sunrise_timestamp)
+    sunset_date = fromtimestamp(sunset_timestamp)
     return render_template(
         "weather.html",
         title=weather_data["name"],
@@ -42,8 +47,8 @@ def get_weather():
         atmos_pressure=f"{weather_data['main']['pressure']}",
         wind=f"{weather_data['wind']['speed']:.1f}",
         country_code=weather_data["sys"]["country"],
-        sunrise=weather_data["sys"]["sunrise"],
-        sunset=weather_data["sys"]["sunset"],
+        sunrise=sunrise_date.isoformat(),
+        sunset=sunset_date.isoformat(),
         targets_daytime=weather_data["dt"],
         targets_tz=weather_data["timezone"],
         geo_latitude=weather_data["coord"]["lat"],
