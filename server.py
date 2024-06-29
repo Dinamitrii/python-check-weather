31dc54d1de7c1216f471b04af709bd7f2a29b311
dinamitrii <k_karaivanov@abv.bg>
2024-06-29 12:57:52 +0300

    implementing shortly a QR_CODE embedded

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ def get_weather():
     sunset_date = datetime.fromtimestamp(sunset_timestamp)
     targets_date = datetime.fromtimestamp(targets_dt_timestamp)
     targets_tz_hrf = int(targets_tz / 3600)
+
     return render_template(
         "weather.html",
         title=weather_data["name"],
