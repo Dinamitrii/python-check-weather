c4fa4b316851aafd49cdb86df094e9951be70151
dinamitrii <k_karaivanov@abv.bg>
2024-07-17 11:01:57 +0300

    coding now

--- weather.py
+++ weather.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     # Check for empty strings or string with only spaces
     if not bool(city.strip()):
-        city = "Sofia"
+        city = "София"
 
     weather_data = get_current_weather(city)
 
